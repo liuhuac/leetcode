f4a8683cd044f6ccba5e96e0c74773a5be3ca254
liuhuac <chenliuhuazju@gmail.com>
2015-06-13 00:07:15 -0400

    Unique Binary Search Trees

---
+++ src/edu/clemson/ece/leetcode/UniqueBinarySearchTrees/UniqueBinarySearchTrees.java
@@ -0,0 +1,26 @@
+package edu.clemson.ece.leetcode.UniqueBinarySearchTrees;
+
+public class UniqueBinarySearchTrees {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public int numTrees(int n) {
+        int[] t = new int[n+1];
+        t[0] = 1;
+        t[1] = 1;
+        for(int i=2; i<n+1; i++){
+            t[i] = 0;
+            for(int j=0; j<i; j++){
+                t[i] += t[j]*t[i-1-j];
+            }
+        }
+        return t[n];
+    }
+
+}
