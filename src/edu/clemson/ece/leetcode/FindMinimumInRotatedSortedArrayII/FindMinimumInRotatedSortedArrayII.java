9cde1ad569bdc436a8c2a2a2fff7ba9780ef732c
liuhuac <chenliuhuazju@gmail.com>
2015-06-19 01:14:14 -0400

    Find Minimum in Rotated Sorted Array II

--- src/edu/clemson/ece/leetcode/FindMinimumInRotatedSortedArrayII/FindMinimumInRotatedSortedArrayII.java
+++ src/edu/clemson/ece/leetcode/FindMinimumInRotatedSortedArrayII/FindMinimumInRotatedSortedArrayII.java
@@ -7,4 +7,23 @@ public class FindMinimumInRotatedSortedArrayII {
 
 	}
 
+	public int findMin(int[] nums) {
+        return findMin(nums, 0, nums.length-1);
+    }
+    
+    public int findMin(int[] nums, int l, int r) {
+        if(l==r) return nums[l];
+        if(1==r-l) return Math.min(nums[l], nums[r]);
+        int mid = l + (r - l)/2;
+        while(l<mid && (nums[l]==nums[mid])){ l++; }
+        while(r>mid && (nums[r]==nums[mid])){ r--; }
+        if(l==mid || mid==r) return findMin(nums, l, r);
+        if(nums[l]>nums[mid]){
+            return findMin(nums, l, mid);
+        }
+        if(nums[r]<nums[mid]){
+            return findMin(nums, mid+1, r);
+        }
+        return Math.min(nums[l], nums[mid+1]);
+    }
 }
