579047f48b67a07ceb46b5ac630ef19abb3a69b2
liuhuac <chenliuhuazju@gmail.com>
2015-06-20 22:24:19 -0400

    FlattenBinaryTreeToLinkedList

---
+++ src/edu/clemson/ece/leetcode/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.java
@@ -0,0 +1,13 @@
+package edu.clemson.ece.leetcode.FlattenBinaryTreeToLinkedList;
+
+public class FlattenBinaryTreeToLinkedList {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
