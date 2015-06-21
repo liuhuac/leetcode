e3aa9bd7fd0f13b279b6afcb732a6308d6764bbb
liuhuac <chenliuhuazju@gmail.com>
2015-06-21 00:13:27 -0400

    Flatten Binary Tree to Linked List

--- src/edu/clemson/ece/leetcode/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.java
+++ src/edu/clemson/ece/leetcode/FlattenBinaryTreeToLinkedList/FlattenBinaryTreeToLinkedList.java
@@ -5,6 +5,19 @@ public class FlattenBinaryTreeToLinkedList {
 	/**
 	 * @param args
 	 */
+	TreeNode pre;
+    public void flatten(TreeNode root) {
+        if(null==root) return;
+        pre = root;
+        flatten(root.left);
+        pre.right = root.right;
+        flatten(root.right);
+        if(null!=root.left){
+            root.right = root.left;
+            root.left = null;
+        }
+    }
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
