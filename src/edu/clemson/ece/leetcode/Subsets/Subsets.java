992d07b6be5f0cdc8e88b4a298decaca39f29b2f
liuhuac <chenliuhuazju@gmail.com>
2015-06-21 20:15:02 -0400

    Subsets

---
+++ src/edu/clemson/ece/leetcode/Subsets/Subsets.java
@@ -0,0 +1,52 @@
+package edu.clemson.ece.leetcode.Subsets;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subsets {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Subsets ss = new Subsets();
+		int[] nums = new int[]{1,2};
+		List<List<Integer>> res = ss.subsets(nums);
+		for(List<Integer> l : res){
+			System.out.println(l.toString());
+		}
+	}
+
+	public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        res.add(new ArrayList<Integer>());
+        for(int i=1; i<=nums.length; i++){
+            List<List<Integer>> tmp = subsets(nums, i, nums.length);
+            res.addAll(tmp);
+        }
+        
+        return res;
+    }
+    public List<List<Integer>> subsets(int[] nums, int k, int n) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(0==k) return res;
+        if(k==n){
+            for(int i=0; i<n; i++){
+                List<Integer> tmp = new ArrayList<Integer>();
+                tmp.add(nums[i]);
+                res.add(tmp);
+            }
+            return res;
+        }
+        List<List<Integer>> pre1 = subsets(nums, k-1, n-1);
+        List<List<Integer>> pre2 = subsets(nums, k, n-1);
+        res.addAll(pre2);
+        for(List<Integer> l : pre1){
+            l.add(nums[n-1]);
+            res.add(l);
+        }
+        return res;
+    }
+    
+}
