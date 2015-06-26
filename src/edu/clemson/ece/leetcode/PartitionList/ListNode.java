562167b278abe1797ec913ffcd460477098e1bc7
liuhuac <chenliuhuazju@gmail.com>
2015-06-26 09:41:02 -0400

    Partition List Memory Limit Exceeded

---
+++ src/edu/clemson/ece/leetcode/PartitionList/ListNode.java
@@ -0,0 +1,10 @@
+package edu.clemson.ece.leetcode.PartitionList;
+
+public class ListNode {
+	int val;
+	 ListNode next;
+	 ListNode(int x) {
+		 val = x;
+	     next = null;
+	 }
+}
---
+++ src/edu/clemson/ece/leetcode/PartitionList/MemoryLimitExceeded.java
@@ -0,0 +1,43 @@
+package edu.clemson.ece.leetcode.PartitionList;
+
+public class MemoryLimitExceeded {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode partition(ListNode head, int x) {
+        ListNode scan = head;
+        ListNode small = null;
+        ListNode large = null;
+        ListNode send = null;
+        ListNode lend = null;
+        while(null!=scan){
+            if(scan.val<x){
+                if(null==small) small = scan;
+                if(null==send){
+                    send = scan;
+                } else {
+                    send.next = scan;
+                    send = scan;
+                }
+            } else {
+                if(null==large) large = scan;
+                if(null==lend){
+                    lend = scan;
+                } else {
+                    lend.next = scan;
+                    lend = scan;
+                }
+            }
+            scan = scan.next;
+        }
+        if(null==send) return large;
+        send.next = large;
+        return small;
+    }
+}
