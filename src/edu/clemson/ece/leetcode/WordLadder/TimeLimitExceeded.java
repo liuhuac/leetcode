a16bfcf76be4fcafa75edb74fcfe1fe5b5eb243f
liuhuac <chenliuhuazju@gmail.com>
2015-07-05 18:04:01 -0400

    Word Ladder Time Limit Exceeded

--- src/edu/clemson/ece/leetcode/WordLadder/TimeLimitExceeded.java
+++ src/edu/clemson/ece/leetcode/WordLadder/TimeLimitExceeded.java
@@ -5,14 +5,18 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Set;
 
-public class WordLadder {
+public class TimeLimitExceeded {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		TimeLimitExceeded wl = new TimeLimitExceeded();
+		Set<String> wordDict = new HashSet<>();
+		wordDict.add("hot");
+		wordDict.add("dog");
+		System.out.println(wl.ladderLength("hot", "dog", wordDict));
 	}
 	public int ladderLength(String beginWord, String endWord, Set<String> wordDict) {
         Queue<String> queue = new LinkedList<>();
@@ -20,26 +24,29 @@ public class WordLadder {
         int n = 0;
         queue.add(beginWord);
         while(!queue.isEmpty()){
-            String str = queue.remove();
             n++;
             Queue<String> tmp = new LinkedList<>();
             while(!queue.isEmpty()){
-                
-            }
-            for(String s : wordDict){
-                if(used.contains(s)) continue;
-                if(isAdjacent(str,s)){
-                    queue.add(s);
-                    used.add(s);
+                String str = queue.remove();
+                if(isAdjacent(str,endWord)){
+                    return n + 1;
+                }
+                for(String s : wordDict){
+                    if(used.contains(s)) continue;
+                    if(isAdjacent(str,s)){
+                        tmp.add(s);
+                        used.add(s);
+                    }
                 }
             }
+            queue = tmp;
         }
-        
+        return 0;
     }
     public boolean isAdjacent(String word1, String word2){
         int diff = 0;
         for(int i=0; i<word1.length(); i++){
-            if(word1.charAt(i)!=word1.charAt(i)) diff++;
+            if(word1.charAt(i)!=word2.charAt(i)) diff++;
             if(2==diff) return false;
         }
         return true;
