7f2414d10648a12d98583f8f7ec62bedd469fb94
liuhuac <chenliuhuazju@gmail.com>
2015-07-12 22:26:27 -0400

    Populating Next Right Pointers in Each Node II

--- src/edu/clemson/ece/leetcode/PopulatingNextRightPointersInEachNodeII/PopulatingNextRightPointersInEachNodeII.java
+++ src/edu/clemson/ece/leetcode/PopulatingNextRightPointersInEachNodeII/PopulatingNextRightPointersInEachNodeII.java
@@ -9,5 +9,27 @@ public class PopulatingNextRightPointersInEachNodeII {
 		// TODO Auto-generated method stub
 
 	}
-
+	public void connect(TreeLinkNode root) {
+        if(null==root) return;
+        connect(root.left);
+        connect(root.right);
+        TreeLinkNode left = root.left;
+        TreeLinkNode right = root.right;
+        while(null!=left&&null!=right){
+            TreeLinkNode rightmost = left;
+            TreeLinkNode leftmost = right;
+            while(null!=rightmost.next) rightmost = rightmost.next;
+            left = next(left);
+            right = next(right);
+            rightmost.next = leftmost;
+        }
+    }
+    public TreeLinkNode next(TreeLinkNode root) {
+        while(null==root.left&&null==root.right) {
+            if(null==root.next) return null;
+            root = root.next;
+        }
+        if(null!=root.left) return root.left;
+        return root.right;
+    }
 }
---
+++ src/edu/clemson/ece/leetcode/PopulatingNextRightPointersInEachNodeII/TreeLinkNode.java
@@ -0,0 +1,9 @@
+package edu.clemson.ece.leetcode.PopulatingNextRightPointersInEachNodeII;
+
+public class TreeLinkNode {
+	
+	int val;
+	TreeLinkNode left, right, next;
+	TreeLinkNode(int x) {val=x;}
+
+}
