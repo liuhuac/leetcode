728089e8fb183d9d44804c9a98356af1dc521110
liuhuac <chenliuhuazju@gmail.com>
2015-10-21 00:29:56 -0400

    Nim Game

---
+++ src/edu/clemson/ece/leetcode/NimGame/NimGame.java
@@ -0,0 +1,12 @@
+package edu.clemson.ece.leetcode.NimGame;
+
+public class NimGame {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public boolean canWinNim(int n) {
+        return n%4==0 ? false : true;
+    }
+}
