223ec39eeb593dcfceea1cf100681d259bf2d6ff
liuhuac <chenliuhuazju@gmail.com>
2015-10-27 20:23:48 -0400

    Add and Search Word - Data structure design

--- src/edu/clemson/ece/leetcode/AddAndSearchWord/WordDictionary.java
+++ src/edu/clemson/ece/leetcode/AddAndSearchWord/WordDictionary.java
@@ -26,22 +26,15 @@ public class WordDictionary {
     }
     
     public boolean search(String word, Node node) {
-        for(int i=0; i<word.length(); i++){
-            char c = word.charAt(i);
-            if(c=='.'){
-                for(Node n : node.nodes){
-                    if(search(word.substring(i+1), n)){
-                        return true;
-                    } 
-                }
-                return false;
-            } else {
-                if(node.nodes[c-'a']==null){
-                    return false;
-                } else {
-                    return search(word.substring(i+1), node.nodes[c-'a']);
-                }
+        if(word.length()==0) return node.isLeaf;
+        char c = word.charAt(0);
+        if(c=='.'){
+            for(Node n : node.nodes){
+                if(n==null) continue;
+                if(search(word.substring(1), n)) return true;
             }
+        } else {
+            if(node.nodes[c-'a']!=null) return search(word.substring(1), node.nodes[c-'a']);
         }
         return false;
     }
