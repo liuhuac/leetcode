02feb33a6d44e11649ba5895077a1a5fe4b04942
liuhuac <chenliuhuazju@gmail.com>
2015-11-05 21:36:02 -0500

    Longest Valid Parentheses

--- src/edu/clemson/ece/leetcode/LongestValidParentheses/LongestValidParentheses.java
+++ src/edu/clemson/ece/leetcode/LongestValidParentheses/LongestValidParentheses.java
@@ -9,5 +9,40 @@ public class LongestValidParentheses {
 		// TODO Auto-generated method stub
 
 	}
-
+	public int longestValidParentheses(String s) {
+        int num = 0;
+        int max = 0;
+        int cur = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)=='('){
+                num++;
+                cur++;
+            } else {
+                if(num>0){
+                    num--;
+                    cur++;
+                    if(num==0&&max<cur) max =  cur;
+                } else {
+                    cur = 0;
+                }
+            }
+        }
+        num = 0;
+        cur = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i)==')'){
+                num++;
+                cur++;
+            } else {
+                if(num>0){
+                    num--;
+                    cur++;
+                    if(num==0&&max<cur) max =  cur;
+                } else {
+                    cur = 0;
+                }
+            }
+        }
+        return max;
+    }
 }
