0a2279f4b3da6b33bce30d6394ffcbe3a9309b69
liuhuac <chenliuhuazju@gmail.com>
2015-11-08 17:19:58 -0500

    Recover Binary Search Tree

---
+++ src/edu/clemson/ece/leetcode/RecoverBinarySearchTree/RecoverBinarySearchTree.java
@@ -0,0 +1,30 @@
+package edu.clemson.ece.leetcode.RecoverBinarySearchTree;
+
+public class RecoverBinarySearchTree {
+
+	/**
+	 * @param args
+	 */
+	TreeNode fst = null;
+    TreeNode snd = null;
+    TreeNode pre = new TreeNode(Integer.MIN_VALUE);
+    public void recoverTree(TreeNode root) {
+        inOrder(root);
+        int tmp = snd.val;
+        snd.val = fst.val;
+        fst.val = tmp;
+    }
+    public void inOrder(TreeNode root){
+        if(root==null) return;
+        inOrder(root.left);
+        if(fst==null&&pre.val>root.val) fst = pre;
+        if(fst!=null&&pre.val>root.val) snd = root;
+        pre = root;
+        inOrder(root.right);
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
