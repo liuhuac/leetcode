c4375fac18ab2e2f351912f4bcb2ceea5cbb89eb
liuhuac <chenliuhuazju@gmail.com>
2015-11-08 22:43:05 -0500

    N-Queens

--- src/edu/clemson/ece/leetcode/NQueens/NQueens.java
+++ src/edu/clemson/ece/leetcode/NQueens/NQueens.java
@@ -8,87 +8,57 @@ public class NQueens {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public List<String[]> solveNQueens(int n) {
-        int[] pos = new int[n];
-        for(int i=0; i<n; i++) pos[i] = i;
-        
-        List<String[]> list = new ArrayList<String[]>();
-        int times = factorial(n);
-        for(int i=0; i<times-1; i++){
-            if(isSolution(pos)) list.add(solutionToStringArray(pos));
-            pos = nextPer(pos);
-        }
-        
-        return list;
+	List<List<String>> ans = new ArrayList<>();
+    public List<List<String>> solveNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i=0; i<board.length; i++)
+            for(int j=0; j<board[0].length; j++) 
+                board[i][j] = '.';
+        dfs(board,0);
+        return ans;
     }
-    
-    public boolean isSolution(int[] pos){
-        int n = pos.length;
-        for(int i=0; i<n; i++){
-            for(int k=1; k<n; k++){
-                if( Math.abs(pos[(n+i-k)%n]-pos[i])<k || Math.abs(pos[(i+k)%n]-pos[i])<k){
-                    return false;
-                }
+    public void dfs(char[][] board, int row){
+        if(row==board[0].length){
+            ans.add(toListString(board));
+            return;
+        }
+        for(int j=0; j<board[0].length; j++){
+            if(isSafe(board,row,j)){
+                board[row][j] = 'Q';
+                dfs(board,row+1);
+                board[row][j] = '.';
             }
         }
+    }
+    public boolean isSafe(char[][] board, int row, int col){
+        for(int i=0; i<board.length; i++) 
+            if( board[row][i]=='Q' ||
+                board[i][col]=='Q' ||
+                
+                row-i>=0&&col-i>=0&&board[row-i][col-i]=='Q' ||
+                row+i<board.length&&col+i<board.length&&board[row+i][col+i]=='Q' ||
+                
+                row-i>=0&&col+i<board.length&&board[row-i][col+i]=='Q' ||
+                row+i<board.length&&col-i>=0&&board[row+i][col-i]=='Q')
+                    return false;
         return true;
     }
-    
-    public int[] nextPer(int[] pos){
-        int n = pos.length;
-        int comp = pos[pos.length-1];
-        int l = n - 2;
-        int r = 0;
-        while(0<l){
-            if(pos[l]<comp){
-                r = l;
-                while(n>++r){
-                    if(pos[r]<pos[l]){
-                        pos = swap(pos, l, r);
-                        break;
-                    }
-                }
-                break;
-            }
-            comp = pos[l--];
+    public List<String> toListString(char[][] board){
+        List<String> ls = new ArrayList<>();
+        for(int i=0; i<board.length; i++){
+            String str = "";
+            for(int j=0; j<board[0].length; j++) str += board[i][j];
+            ls.add(str);
         }
-        return quicksort(pos, l+1, n-1);
+        return ls;
     }
-    
-    public int[] swap(int[] pos, int l, int r){
-        int tmp = pos[l];
-        pos[l] = pos[r];
-        pos[r] = tmp;
-        return pos;
-    }
-    
-    public int[] quicksort(int[] pos, int l, int r){
-        
-    }
-    
-    public int factorial(int n){
-        if(0==n) return 1;
-        if(1==n) return 1;
-        return n*factorial(n-1);
-    }
-    
-    public String[] solutionToStringArray(int[] pos){
-        int n = pos.length;
-        String[] sList = new String[n];
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(pos[i]==j){
-                    sList[i] += "Q";
-                } else {
-                    sList[i] += ".";
-                }
-            }
-        }
-        return sList;
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int test1 = 2%3;
+		int test2 = -2%3;
+		int test3 = (-2)%3;
+		NQueens nq = new NQueens();
+		nq.solveNQueens(4);
+	}
+
 }
---
+++ src/edu/clemson/ece/leetcode/NQueens/NQueensPermutation.java
@@ -0,0 +1,94 @@
+package edu.clemson.ece.leetcode.NQueens;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NQueensPermutation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<String[]> solveNQueens(int n) {
+        int[] pos = new int[n];
+        for(int i=0; i<n; i++) pos[i] = i;
+        
+        List<String[]> list = new ArrayList<String[]>();
+        int times = factorial(n);
+        for(int i=0; i<times-1; i++){
+            if(isSolution(pos)) list.add(solutionToStringArray(pos));
+            pos = nextPer(pos);
+        }
+        
+        return list;
+    }
+    
+    public boolean isSolution(int[] pos){
+        int n = pos.length;
+        for(int i=0; i<n; i++){
+            for(int k=1; k<n; k++){
+                if( Math.abs(pos[(n+i-k)%n]-pos[i])<k || Math.abs(pos[(i+k)%n]-pos[i])<k){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    
+    public int[] nextPer(int[] pos){
+        int n = pos.length;
+        int comp = pos[pos.length-1];
+        int l = n - 2;
+        int r = 0;
+        while(0<l){
+            if(pos[l]<comp){
+                r = l;
+                while(n>++r){
+                    if(pos[r]<pos[l]){
+                        pos = swap(pos, l, r);
+                        break;
+                    }
+                }
+                break;
+            }
+            comp = pos[l--];
+        }
+        return quicksort(pos, l+1, n-1);
+    }
+    
+    public int[] swap(int[] pos, int l, int r){
+        int tmp = pos[l];
+        pos[l] = pos[r];
+        pos[r] = tmp;
+        return pos;
+    }
+    
+    public int[] quicksort(int[] pos, int l, int r){
+        
+    }
+    
+    public int factorial(int n){
+        if(0==n) return 1;
+        if(1==n) return 1;
+        return n*factorial(n-1);
+    }
+    
+    public String[] solutionToStringArray(int[] pos){
+        int n = pos.length;
+        String[] sList = new String[n];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(pos[i]==j){
+                    sList[i] += "Q";
+                } else {
+                    sList[i] += ".";
+                }
+            }
+        }
+        return sList;
+    }
+}
