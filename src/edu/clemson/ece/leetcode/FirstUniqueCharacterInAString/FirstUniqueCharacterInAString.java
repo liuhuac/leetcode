7d3a3fc376770d2877d94431b6fa0c27465884e9
Liuhua Chen <chenliuhuazju@gmail.com>
2016-08-22 23:55:08 -0400

    First unique character in a string

---
+++ src/edu/clemson/ece/leetcode/FirstUniqueCharacterInAString/FirstUniqueCharacterInAString.java
@@ -0,0 +1,12 @@
+package edu.clemson.ece.leetcode.FirstUniqueCharacterInAString
+
+public class FirstUniqueCharacterInAString {
+    public int firstUniqChar(String s) {
+        int[] frq = new int[26];
+        for(int i=0; i<s.length(); i++) frq[s.charAt(i)-'a']++;
+        for(int i=0; i<s.length(); i++){
+            if(frq[s.charAt(i)-'a']==1) return i;
+        }
+        return -1;
+    }
+}
