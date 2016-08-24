d88122c202e6c5af1df97080057e34fcdefebd66
Liuhua Chen <chenliuhuazju@gmail.com>
2016-08-24 12:04:59 -0400

    Sum of two integers

---
+++ src/edu/clemson/ece/leetcode/SumOfTwoIntegers/SumOfTwoIntegers.java
@@ -0,0 +1,12 @@
+package edu.clemson.ece.leetcode.SumOfTwoIntegers;
+
+public class SumOfTwoIntegers {
+    public int getSum(int a, int b) {
+        while(b!=0){
+            int carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
