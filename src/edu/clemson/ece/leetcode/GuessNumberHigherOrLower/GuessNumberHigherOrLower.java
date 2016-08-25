5cdd8c0857e604291991c1d006bdf5542c9764a8
Liuhua Chen <chenliuhuazju@gmail.com>
2016-08-25 16:35:00 -0400

    Guess number higher or lower

--- src/edu/clemson/ece/leetcode/GuessNumberHigherOrLower/GuessNumberHigherOrLower.java
+++ src/edu/clemson/ece/leetcode/GuessNumberHigherOrLower/GuessNumberHigherOrLower.java
@@ -1,37 +1,26 @@
 package edu.clemson.ece.leetcode.GuessNumberHigherOrLower;
 
-//Forward declaration of guess API.
-//@param num, your guess
-//@return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+/* The guess API is defined in the parent class GuessGame.
+ * @param num, your guess
+ * @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+ * int guess(int num); */
 
-
-class GuessNumberHigherOrLower {
-	int guess(int num){
-		int target = 6;
-		if(num>target) return 1;
-		if(num<target) return -1;
-		if(num==target) return 0;
-	}
-public:
-	static void main(int[] args){
-		GuessNumberHigherOrLower test = new GuessNumberHigherOrLower();
-		test.guessNumber(10);
-	}
- int guessNumber(int n) {
-     int lo = 1;
-     int hi = n;
-     int mid = 0;
-     while(lo<hi){
-         mid = lo + (hi-lo)/2;
-         int result = guess(mid);
-         if(result==1){
-             hi = mid;
-         } else if(result==-1){
-             lo = mid;
-         } else {
-             break;
+public class GuessNumberHigherOrLower extends GuessGame {
+    public int guessNumber(int n) {
+         int lo = 1;
+         int hi = n;
+         /* int mid = lo + (hi-lo)/2; */
+         while(lo<hi){
+             int mid = lo + (hi-lo)/2;
+             int result = guess(mid);
+             if(result==-1){
+                 hi = mid;
+             } else if(result==1){
+                 lo = mid+1;
+             } else {
+                 return mid;
+             }
          }
-     }
-     return mid;
- }
-};
+         return lo;
+        }
+}
